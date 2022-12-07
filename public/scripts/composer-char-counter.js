f08a3f6c22279d79608d8b6000b0b72892f0081d
WesleyMatthee <wrmatthee@gmail.com>
2022-12-07 02:25:22 +0000

    this is a long over due commit, I added NavBar, Tweet composer and fake tweets. Will need to commit more

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+  console.log('Tommy can you here');
+
+  $('#tweet-text').on('input', function() {
+    //console.log($(this));
+    let charCount = $(this).val().length; //.val() method getting the value of tweet text
+    let remainingChar = 140 - charCount;
+
+    //DOM tree traversal targeting of the .counter value
+    let counter = $(this).parent().next('div').children('.counter');
+    counter.text(remainingChar);
+
+    if (remainingChar < 0) {
+      counter.addClass('error');
+    } else {
+      counter.removeClass('error');
+    }
+  });
+});
+
