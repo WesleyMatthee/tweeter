3f002126e2ae5c92182e63ff242d70fdaf3527a9
WesleyMatthee <wrmatthee@gmail.com>
2022-12-08 21:20:44 +0000

    add AJAX, POST & GET methods to client.js and import timestamp library in index.html to use time.ago() method in client.js...try to commit more frequently :P

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
         <p>${tweetData.content.text}</p>
         <hr/>
         <footer >
-          <div>${tweetData.created_at}</div>
+          <div>${timeago.format(tweetData.created_at)}</div>
           <div class="icons">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -55,7 +55,7 @@ $(document).ready(function() {
     return $tweet;
   };
 
- 
+
   // takes return value and appends it to the tweets container
   const renderTweets = function(tweets) {
     // loops through tweets
@@ -65,6 +65,44 @@ $(document).ready(function() {
     }
   };
 
+  const loadTweets = function(tweets) {
+    $.ajax({
+      type: "GET",
+      url: "/tweets",
+      success: function(result) {
+        console.log(result);
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    });
+  };
+
+
+  //Form Submission using jQuery
+  $('.tweet-form').submit(function(event) { //Avoid arrow functions whe using AJAX
+    event.preventDefault();
+    console.log('Hello from jquery form!');
+    let data = $(this).serialize();
+    console.log(data);
+    //This is a great way to use AJAX for a POST & GET request
+    $.ajax({
+      type: "POST", // 1: type of request
+      url: "/tweets", // 2: location to POST or GET
+      data: data, // 3: The Data that is being POSTED
+      success: function(result) { //SUCCESS FUNCTION for good result
+        console.log(result);
+        loadTweets();
+      },
+      error: function(err) { //ERROR FUNCTION incase unsuccessful 
+        console.log(err);
+      }
+    });
+
+   
+
+
+  });
 
 
   renderTweets(tweets);
